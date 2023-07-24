d167c3d06be980220afb950dca4a33464bc49be1
Alex Beng <599267065@qq.com>
2023-07-25 00:22:06 +0800

    add export_all_list.py

---
+++ export_all_list.py
@@ -0,0 +1,11 @@
+from mona.text import ALL_NAMES
+import json
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print('Usage: python export_all_list.py [output_path]')
+    exit(1)
+pt = sys.argv[1]
+path =  os.path.join(pt, 'all_list.json')
+json.dump(ALL_NAMES, open(path, 'w', encoding='utf-8'), ensure_ascii=False)
