681e3cec694b8caf4cd48f1089b6737f8aa9ca60
Alex Beng <599267065@qq.com>
2023-08-27 22:38:20 +0800

    make exported onnx model batch-size change-able

--- to_onnx.py
+++ to_onnx.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision.transforms as T
+from torchsummary import summary
 import torch.onnx
 import onnx
 import onnxruntime
@@ -9,50 +10,70 @@ import json
 from mona.nn.model import Model
 from mona.nn.model2 import Model2
 from mona.text import word_to_index, index_to_word
-from mona.datagen.datagen import generate_image
+from mona.datagen.datagen import generate_image,generate_mix_image
 
 
+model_folder = "models"
 # name = "model_training.pt"
-name = "model_acc100-epoch45.pt"
+name = "model_acc100-epoch265.pt"
 onnx_name = name.rsplit(".", 2)[0] + ".onnx"
 # net = Model(len(word_to_index))
 net = Model2(len(word_to_index), in_channels=1)
 
-net.load_state_dict(torch.load(f"models/{name}"))
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+net.load_state_dict(torch.load(f"{model_folder}/{name}", map_location=torch.device(device)))
 net.eval()
 
+def get_parameter_number(model):
+    total_num = sum(p.numel() for p in model.parameters())
+    trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+print(get_parameter_number(net))
+# exit()
+summary(net, (1, 32, 384))
+
+
 to_tensor = T.ToTensor()
-x, label = generate_image()
+x, label = generate_mix_image()
+x2, label2 = generate_mix_image()
 x = to_tensor(x)
+x2 = to_tensor(x2)
+x = torch.stack([x, x2, x, x, x], dim=0)
 print(x.shape)
-x.unsqueeze_(0) # (1, 3, 32, width)
+# x.unsqueeze_(0) # (1, 3, 32, width)
 y = net(x)      # (width / 8, 1, lexicon_size)
 print(x.size(), y.size())
 
 torch.onnx.export(
     net,
     x,
-    f"models/{onnx_name}",
+    f"{model_folder}/{onnx_name}",
     export_params=True,
     opset_version=11,
     do_constant_folding=True,
     input_names=["input"],
     output_names=["output"],
     dynamic_axes={
-        "input": {3: "image_width"},
+        "input": {3: "image_width", 0: "image_num"},
         "output": {0: "seq_length"},
     }
 )
 
-onnx_model = onnx.load(f"models/{onnx_name}")
+onnx_model = onnx.load(f"{model_folder}/{onnx_name}")
 onnx.checker.check_model(onnx_model)
 
-ort_session = onnxruntime.InferenceSession(f"models/{onnx_name}")
+ort_session = onnxruntime.InferenceSession(f"{model_folder}/{onnx_name}")
 
+
+# print(ort_session.get_inputs()[0])
 ort_inputs = {ort_session.get_inputs()[0].name: x.numpy()}
+
 ort_outs = ort_session.run(None, ort_inputs)
+# print()
+# print(ort_outs[0].shape)
 np.testing.assert_allclose(y.detach().numpy(), ort_outs[0], rtol=1e-3, atol=1e-5)
 
-with open("models/index_2_word.json", "w", encoding="utf-8") as f:
+with open(f"{model_folder}/index_2_word.json", "w", encoding="utf-8") as f:
     j = json.dumps(index_to_word, indent=4, ensure_ascii=False).encode("utf8")
     f.write(j.decode())
