059045963996614515018669c376d20c877f2347
Alex Beng <599267065@qq.com>
2023-08-27 23:30:06 +0800

    update config

--- mona/config.py
+++ mona/config.py
@@ -1,19 +1,19 @@
 config = {
     "height": 32,
     "train_width": 384,
-    "batch_size": 128,
-    "epoch": 50,
-    "print_per": 100,
-    "save_per": 600,
+    "batch_size": 512, # 550 on 4GB gpu memo
+    "epoch": 1000,
+    "print_per": 10,
+    "save_per": 800,
 
     "train_size": 200000,
-    "validate_size": 10000,
+    "validate_size": 30000,
 
-    "pretrain": False,
-    "pretrain_name": "chs_all.pt",
+    "pretrain": True,
+    "pretrain_name": "model_training.pt",
 
     # Set according to your CPU
-    "dataloader_workers": 1,
+    "dataloader_workers": 3,
     # Generate data online for train/val
     "online_train": True,
     "online_val": True,
