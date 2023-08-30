27c0944c76ed3a02e8a429551673c6e2a9dc899b
Alex Beng <599267065@qq.com>
2023-08-30 23:52:03 +0800

    add export npc&domain&ops script

---
+++ export_bw_list.py
@@ -0,0 +1,31 @@
+# from mona.text import bk_list
+from mona.text.characters import characters_name
+from mona.text.operations import operations_names
+from mona.text.domains import domain_names
+
+import json
+import sys
+import os
+
+def read_or_create_json(path: str):
+    if os.path.exists(path):
+        return json.load(open(path, 'r', encoding='utf-8'))
+    else:
+        return []
+
+if len(sys.argv) < 2:
+    print('Usage: python export_bw_list.py [output_path]')
+    exit(1)
+pt = sys.argv[1]
+path =  os.path.join(pt, 'black_lists.json')
+black_lists = read_or_create_json(path)
+# print(black_lists)
+
+# merge the npc and domain and op name to balck lists
+all_bk = characters_name + operations_names + domain_names
+for name in all_bk:
+    if name not in black_lists:
+        black_lists.append(name)
+print(black_lists)
+json.dump(black_lists, open(path, 'w', encoding='utf-8'), ensure_ascii=False)
+
