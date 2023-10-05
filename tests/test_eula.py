50700e1673d2e201a307e5e8e832a6be5d96cacf
Alex Beng <599267065@qq.com>
2023-10-05 22:07:58 +0800

    add trainalbe count to test eula

--- tests/test_eula.py
+++ tests/test_eula.py
@@ -1,9 +1,17 @@
 import torch
 
+import os
+import sys
+sys.path.append(os.getcwd())
 
-from eula.nn.backbone import MobileNetV3Small
-from eula.nn.head import MobileNetV3Small_head
+
 from eula.nn.centernet import CenterNet_MobilenetV3Small
+
+def get_parameter_number(model):
+    total_num = sum(p.numel() for p in model.parameters())
+    trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
 
 if __name__ == "__main__":
     model = CenterNet_MobilenetV3Small(num_classes=10)
@@ -11,3 +19,4 @@ if __name__ == "__main__":
     x = torch.randn(1, 3, 64, 384)
     cls, offset = model(x)
     print(cls.shape, offset.shape)
+    print(get_parameter_number(model))
