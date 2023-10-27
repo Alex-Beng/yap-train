c546e1faa0a0bdd61e4f8551cfa66662124e445b
Alex Beng <599267065@qq.com>
2023-10-28 04:50:11 +0800

    update & add freeze backbone config

--- mona/config.py
+++ mona/config.py
@@ -2,18 +2,18 @@ config = {
     "height": 32,
     "train_width": 384,
     "batch_size": 256, # 550 on 4GB gpu memo
-    "epoch": 1000,
+    "epoch": 1000000,
     "print_per": 10,
     "save_per": 200,
 
-    "train_size": 200000,
-    "validate_size": 30000,
+    "train_size": 2560,
+    "validate_size": 25600,
 
     "pretrain": True,
     "pretrain_name": "model_training.pt",
 
     # Set according to your CPU
-    "dataloader_workers": 1,
+    "dataloader_workers": 3,
     # Generate data online for train/val
     "online_train": True,
     "online_val": True,
@@ -21,5 +21,10 @@ config = {
     # data distribution: genshin / genshin+word 
     "data_only_genshin": True,
     # 生成混合数据时标注数据比例
-    "data_genshin_ratio": 0.3,
+    "data_genshin_ratio": 0.15,
+
+    # backbone freeze
+    # 初始训练时冻结backbone，训练一段时间后解冻
+    "freeze_backbone": False,
+    "unfreeze_backbone_epoch": 200,
 }
