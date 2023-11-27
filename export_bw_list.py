89e54284cdbdd7f695578a7587beb417ec9cd25d
Alex Beng <599267065@qq.com>
2023-11-27 14:16:54 +0800

    export to default black list

--- export_bw_list.py
+++ export_bw_list.py
@@ -3,6 +3,7 @@ from mona.text.characters import characters_name
 from mona.text.operations import operations_names
 from mona.text.domains import domain_names
 from mona.text.server_leak_names import server_leak_names
+from mona.text.field_operations import field_operations_names
 
 import json
 import sys
@@ -18,12 +19,12 @@ if len(sys.argv) < 2:
     print('Usage: python export_bw_list.py [output_path]')
     exit(1)
 pt = sys.argv[1]
-path =  os.path.join(pt, 'black_lists.json')
+path =  os.path.join(pt, 'default_black_list.json')
 black_lists = read_or_create_json(path)
 # print(black_lists)
 
 # merge the npc and domain and op name to balck lists
-all_bk = characters_name + operations_names + domain_names + server_leak_names
+all_bk = characters_name + operations_names + domain_names + server_leak_names + field_operations_names
 for name in all_bk:
     if name not in black_lists:
         black_lists.append(name)
