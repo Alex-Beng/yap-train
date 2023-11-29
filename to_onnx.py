d84547eefff7034ee7682718dbd884f780e8a427
Alex Beng <599267065@qq.com>
2023-11-30 01:00:14 +0800

    update to onnx

--- to_onnx.py
+++ to_onnx.py
@@ -10,13 +10,15 @@ import json
 from mona.nn.model import Model
 from mona.nn.model2 import Model2
 from mona.text import word_to_index, index_to_word
-from mona.datagen.datagen import generate_image,generate_mix_image
+from mona.datagen.datagen import generate_pure_bg_image,generate_pickup_image, generate_mix_image
 
 
 model_folder = "models"
-# name = "model_training.pt"
-name = "model_acc100-epoch265.pt"
+name = "model_training.pt"
+# name = "model_acc100-epoch860.pt"
+# name = "model_acc9999-epoch800.pt"
 onnx_name = name.rsplit(".", 2)[0] + ".onnx"
+onnx_name = "model_training.onnx"
 # net = Model(len(word_to_index))
 net = Model2(len(word_to_index), in_channels=1)
 
@@ -31,18 +33,20 @@ def get_parameter_number(model):
     return {'Total': total_num, 'Trainable': trainable_num}
 print(get_parameter_number(net))
 # exit()
-summary(net, (1, 32, 384))
+# summary(net, (1, 32, 384))
 
 
 to_tensor = T.ToTensor()
-x, label = generate_mix_image()
-x2, label2 = generate_mix_image()
+x, label = generate_pickup_image()
+x2, label2 = generate_pickup_image()
 x = to_tensor(x)
 x2 = to_tensor(x2)
 x = torch.stack([x, x2, x, x, x], dim=0)
+
 print(x.shape)
 # x.unsqueeze_(0) # (1, 3, 32, width)
 y = net(x)      # (width / 8, 1, lexicon_size)
+# y3 = net(x3)
 print(x.size(), y.size())
 
 torch.onnx.export(
@@ -69,7 +73,11 @@ ort_session = onnxruntime.InferenceSession(f"{model_folder}/{onnx_name}")
 # print(ort_session.get_inputs()[0])
 ort_inputs = {ort_session.get_inputs()[0].name: x.numpy()}
 
+import time
+beg_time = time.time()
 ort_outs = ort_session.run(None, ort_inputs)
+
+print(f"onnxruntime inference time: {time.time() - beg_time}")
 # print()
 # print(ort_outs[0].shape)
 np.testing.assert_allclose(y.detach().numpy(), ort_outs[0], rtol=1e-3, atol=1e-5)
